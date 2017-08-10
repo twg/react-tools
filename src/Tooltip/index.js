ec8eda1a8f5834fd52b7d23210f2599fcc8fd4fb
Hunter Jansen <rawkamatic@gmail.com>
2017-08-10 16:26:18 -0400

    Update component structure to agreed upon export

--- src/Tooltip/index.js
+++ src/Tooltip/index.js
@@ -1,1 +1,40 @@
-export { default as Tooltip } from './Tooltip.js'
+import React, { PropTypes } from 'react'
+import css from './style.css'
+
+const Tooltip = props => {
+  const tooltipClasses = [css.tooltip].join(' ')
+
+  const popoverClasses = [css.popover, props.openLeft ? css.openLeft : ''].join(
+    ' '
+  )
+
+  return (
+    <div className={tooltipClasses}>
+      <span className={css.trigger}>
+        {props.children}
+      </span>
+      <div className={popoverClasses}>
+        {props.content}
+      </div>
+    </div>
+  )
+}
+
+Tooltip.propTypes = {
+  /**
+   * This is the content of the tooltip itself.
+   */
+
+  content: PropTypes.node,
+  /**
+   * This is the content which, on hover, will cause a tooltip to be displayed.
+   */
+
+  children: PropTypes.node,
+  /**
+   * Sets the tooltip to open to the left instead of above
+   */
+  openLeft: PropTypes.bool
+}
+
+export { Tooltip }
