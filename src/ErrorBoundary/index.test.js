eb74be1239f53880bbf201560589d0c38569f9b1
Nicole Chung <nicole.chung@gmail.com>
2017-10-17 09:10:42 -0400

    adding Error Boundary test

--- src/ErrorBoundary/index.test.js
+++ src/ErrorBoundary/index.test.js
@@ -1,8 +1,21 @@
 /* eslint-env jest */
 
 import React from 'react'
-import ErrorBoundary from './ErrorBoundary'
+import renderer from 'react-test-renderer'
+import { ErrorBoundary } from './'
 
-describe('ErrorBoundary', () => {
-  ;<ErrorBoundary />
+const ErrorView = () => {
+  return null
+}
+
+describe('Error Boundary', () => {
+  it('matches snapshot', () => {
+    let component = renderer.create(
+      <ErrorBoundary>
+        <ErrorView />
+      </ErrorBoundary>
+    )
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
 })
