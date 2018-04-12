69f850b42ed53c3b79c514c29c0375d214c4efac
Hyperwidget <rawkamatic@gmail.com>
2018-04-12 11:04:41 -0400

    wip

--- bin/scripts/generateApp.js
+++ bin/scripts/generateApp.js
@@ -1,5 +1,6 @@
 const _ = require('lodash')
 const replace = require('replace-in-file')
+const execSync = require('child_process').execSync
 
 const copy = require('./copy')
 
@@ -18,10 +19,10 @@ const generateComponent = async ({
     __NAME__: name
   }
   const basePath = `${__dirname}/../templates/app`
-  const targetPath = `${target}/${names.__UPPER_CAMEL_NAME__}`
+  const targetPath = `${__dirname}/${target}/${names.__KEBAB_NAME__}`
 
   const options = {
-    files: [`${targetPath}/*.*`],
+    files: [`./*.*`],
     from: [
       /__UPPER_CAMEL_NAME__/g,
       /__LOWER_CAMEL_NAME__/g,
@@ -31,16 +32,36 @@ const generateComponent = async ({
     to: Object.values(names)
   }
 
-  // Start with copying over the base app
-  await copy(`${basePath}/base`, targetPath)
+  // Start with running cra
+  console.log(
+    `Generating ${names.__KEBAB_NAME__} . . . This will take a moment`
+  )
+  process.chdir(target)
+  execSync(`npx create-react-app ${names.__KEBAB_NAME__}`)
 
+  // Ejecting the app
+  console.log(`Preparing for updating ${names.__KEBAB_NAME__} . . .`)
+  process.chdir(names.__KEBAB_NAME__)
+  execSync(`echo y | npm run eject`)
+
+  // Copy src folder from sample app
+  console.log(`Setting up ${targetPath}/src . . .`)
+  execSync(`rm -rf ./src`)
+  await copy(`${basePath}/base/src`, `./src`)
+
+  // Add prettier if needed
   if (prettier) {
-    await copy(`${basePath}/prettier`, targetPath)
+    await copy(`${basePath}/prettier`, './')
+    // execSync(`npx npm-add prettier husky lint-staged`)
+    // Add lint-staged to file
   }
 
+  // Add travis if needed
   if (travis) {
-    await copy(`${basePath}/travis`, targetPath)
+    await copy(`${basePath}/travis`, './')
   }
+
+  // Add other npm modules that are needed
 
   /* Replace template string variables in newly created folder */
   replace.sync(options)
