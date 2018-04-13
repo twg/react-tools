ef9cb151d640a393757100efde46a21a17288060
Hunter Jansen <rawkamatic@gmail.com>
2018-04-13 14:03:02 -0400

    Updated create script (#41)
    * 0.5.1
    
    * wip
    
    * wip
    
    * package.json set up
    
    * dev working
    
    * dev and prod both working
    
    * Fix output package.json format, no longer include unneeded prettier file

--- bin/scripts/generateApp.js
+++ bin/scripts/generateApp.js
@@ -1,5 +1,8 @@
 const _ = require('lodash')
 const replace = require('replace-in-file')
+const execSync = require('child_process').execSync
+const fs = require('fs')
+const packageSort = require('sort-package-json')
 
 const copy = require('./copy')
 
@@ -18,10 +21,14 @@ const generateComponent = async ({
     __NAME__: name
   }
   const basePath = `${__dirname}/../templates/app`
-  const targetPath = `${target}/${names.__UPPER_CAMEL_NAME__}`
+  const targetPath = `${__dirname}/${target}/${names.__KEBAB_NAME__}`
+
+  const deps = JSON.parse(
+    fs.readFileSync(`${basePath}/base/snippets/dependencies.json`)
+  )
 
   const options = {
-    files: [`${targetPath}/*.*`],
+    files: [`./*.*`],
     from: [
       /__UPPER_CAMEL_NAME__/g,
       /__LOWER_CAMEL_NAME__/g,
@@ -31,21 +38,105 @@ const generateComponent = async ({
     to: Object.values(names)
   }
 
-  // Start with copying over the base app
-  await copy(`${basePath}/base`, targetPath)
+  // Start with running cra
+  console.log(
+    `Generating ${names.__KEBAB_NAME__} . . . This will take a moment`
+  )
+  process.chdir(target)
+  execSync(`npx create-react-app ${names.__KEBAB_NAME__}`)
 
+  // Ejecting the app
+  console.log(`Preparing for updating ${names.__KEBAB_NAME__} . . .`)
+  process.chdir(names.__KEBAB_NAME__)
+  execSync(`echo y | npm run eject`)
+
+  // Copy src folder from sample app
+  console.log(`Setting up ${targetPath}/src . . .`)
+  execSync(`rm -rf ./src`)
+  await copy(`${basePath}/base/src`, `./src`)
+  let jsonData = JSON.parse(fs.readFileSync('package.json'))
+
+  jsonData = _.merge(jsonData, deps)
+
+  // Add prettier if needed
   if (prettier) {
-    await copy(`${basePath}/prettier`, targetPath)
+    console.log(`Adding Prettier`)
+    await copy(`${basePath}/prettier/.prettierrc`, './.prettierrc')
+    const prettier = JSON.parse(
+      fs.readFileSync(`${basePath}/prettier/dependencies.json`)
+    )
+    jsonData = _.merge(jsonData, prettier)
   }
 
+  // Add travis if needed
   if (travis) {
-    await copy(`${basePath}/travis`, targetPath)
+    console.log(`Adding Travis`)
+    await copy(`${basePath}/travis`, './')
   }
+
+  // clean up adjusted package.json
+  const sorted = packageSort(jsonData)
+  fs.writeFileSync('package.json', JSON.stringify(sorted, null, 2))
+
+  console.log(`Updating webpack config files`)
+  updateWebpackConfigs()
 
   /* Replace template string variables in newly created folder */
   replace.sync(options)
   console.log(`${names.__UPPER_CAMEL_NAME__} created ✨`)
-  console.log(`Be sure to update your package.json description!`)
+}
+
+const updateWebpackConfigs = () => {
+  // Update webpack config file for sass ++ aliases
+  // This is all pretty brittle, since we're just literall just updating js files
+  // but it works!
+  let webpackDev = fs.readFileSync('config/webpack.config.dev.js', 'utf8')
+  const aliasMarker = 'alias: {'
+  const index = webpackDev.indexOf(aliasMarker) + aliasMarker.length
+  const aliases = `      core: path.resolve(__dirname, '../src/core'),
+  common: path.resolve(__dirname, '../src/common'),
+  styles: path.resolve(__dirname, '../src/common/styles'),`
+  webpackDev = webpackDev.substr(0, index) + aliases + webpackDev.substr(index)
+
+  const cssMarker = '.css'
+  const cssIndex = webpackDev.indexOf(cssMarker) + 1
+  const sassVal = `s?`
+  webpackDev =
+    webpackDev.substr(0, cssIndex) + sassVal + webpackDev.substr(cssIndex)
+
+  const fileMarker = `loader: require.resolve('postcss-loader'),`
+  let fileIndex = webpackDev.indexOf(fileMarker) - 34
+  const loaderVal = `,
+    {
+      loader: require.resolve('sass-loader')
+    }`
+
+  webpackDev =
+    webpackDev.substr(0, fileIndex) + loaderVal + webpackDev.substr(fileIndex)
+
+  fs.writeFileSync('config/webpack.config.dev.js', webpackDev)
+
+  // PROD WEBPACK
+  let webpackProd = fs.readFileSync('config/webpack.config.prod.js', 'utf8')
+  const prodIndex = webpackProd.indexOf(aliasMarker) + aliasMarker.length
+  webpackProd =
+    webpackProd.substr(0, prodIndex) + aliases + webpackProd.substr(prodIndex)
+
+  const prodCssMarker = '.css$'
+  const prodCssIndex = webpackProd.indexOf(prodCssMarker) + 1
+  webpackProd =
+    webpackProd.substr(0, prodCssIndex) +
+    sassVal +
+    webpackProd.substr(prodCssIndex)
+
+  let prodFileIndex = webpackProd.indexOf(fileMarker) - 46
+
+  webpackProd =
+    webpackProd.substr(0, prodFileIndex) +
+    loaderVal +
+    webpackProd.substr(prodFileIndex)
+
+  fs.writeFileSync('config/webpack.config.prod.js', webpackProd)
 }
 
 module.exports = generateComponent
